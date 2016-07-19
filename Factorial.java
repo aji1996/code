ff60151c1c5d341a4e8b63c7177e8873b87121ef
aji1996 <ajithkumartaker@gmail.com>
2016-07-19 09:54:43 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+public static void main(String[] args) {
+	int fact=1;
+	int n;
+	for(int i=1;i<=5;i++){
+		fact=fact*i;
+	}
+	System.out.println(" The  factorial of the given number "+fact);
+}
+	}
